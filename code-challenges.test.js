0fc0738782fa8e030c308f757284a04abada69ab
ElijahBcodes <eli1inthesnow@gmail.com>
2022-09-06 08:58:12 -0700

    assessment comp

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,52 +1,127 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
-
 // Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
-
-// Use test driven development to complete the following questions
-// Add appropriate dependencies: $ yarn add jest
-
-// Reminder: The test will call your function
-// Run the file with the following command: $ yarn jest
-
-// --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
-
+// --------------------1) Create a function that takes in a wordCrypto and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
+// Pseudo code:
+// input: string 
+// transformations: takes the string and 
+// if there is an 'a' it changes it to 4, 
+// if there is an 'e' that will change to 3
+// if there is an 'i' change to 1
+// and if there is an 'o' changes that to 0
+// Output: will be a string but without any a, e,i,o characters instead will have numbers in place of those
+// also will expect that no other characters are altered, and need to take into account capital letters by making them lowercase 
 // a) Create a test with expect statements using the variables provided.
-
-const secretCodeWord1 = "Lackadaisical"
+describe("wordCrypto", () => {
+    it("returns a string letters a, e, i, o converted to numbers",  () => {
+         const secretCodeWord1 = "Lackadaisical";
+         const secretCodeWord2 = "Gobbledygook";
+         const secretCodeWord3 = "Eccentric";       
+        expect(wordCrypto(secretCodeWord1)).toEqual( "L4ck4d41s1c4l");
+        expect(wordCrypto(secretCodeWord2)).toEqual( "G0bbl3dyg00k");
+        expect(wordCrypto(secretCodeWord3)).toEqual( "3cc3ntr1c");
+       
+    });
+  });
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
-
+// FAIL  ./code-challenges.test.js
 // b) Create the function that makes the test pass.
+// // This one below removed the a but how do I add more conditions? to remove other letters? 
+//need if and else if ??
+// const wordCrypto = (str) => {
+//     let newStr= str.replace(/a/gi,4)
+//     return newStr
+// }
+// wordCrypto('Lackadaisical')
+
+// 'L4ck4d4isic4l'
+//more Psuedo: function takes in a string, 
+//Declare variable newStr and replace regex.the g= global i=case insensitive. I didn't know you could chain them up like this, but there has to be a way to get this done without repeating. Is there a way to do it without .replace 4 times?
+//
+
+//
+const wordCrypto = (str) => {
+   let newStr = str.replace(/a/gi, 4).replace(/i/gi, 1).replace(/e/gi, 3).replace(/o/gi, 0)
+        return newStr
+    } 
+    // PASS  ./code-challenges.test.js
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
-
+// Pseudo code 
+// input: an array of words and a single letter 
+// transformations: will take the words and if they match the letter return them
+// output: an array of words that match the letter 
 // a) Create a test with expects statement using the variable provided.
-
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const letterA = "a"
+describe("fruitsOfLabor", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const letterA = "a";
+        const letterE = "e";
+    it("returns an array of all the words containing that particular letter", () => {
+          expect(fruitsOfLabor(fruitArray,letterA )).toEqual(["Mango", "Apricot", "Peach"]);
+          expect(fruitsOfLabor(fruitArray,letterE )).toEqual(["Cherry", "Blueberry", "Peach"])      
+        });
+      });
+    //   FAIL  ./code-challenges.test.js
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
-
 // b) Create the function that makes the test pass.
+//
+// const fruitsOfLabor = (array, str) => { //function that takes in two args the array and the string
+//     let fruits = []                        //declare empty array 
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i].toLowerCase().includes(str)){
+//             fruits.push(array[i])            //if cond met push into fruits variable
+//         }
+//     }
+//     return fruits  
+// }
+//HOF method, takes array and str as args. return them filtered , lowercase the array and if it matches the str then return it//refactor
+const fruitsOfLabor = (array, str) => {
+  return array.filter((array) => array.toLowerCase().includes(str))
+}
+// Pass
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
-
+// Pseudo Code:
+// Input: an array of five numbers 
+// Transformations:determine if there is 3 of a kind and two of any kind aka full house
+// check for three of the same of any digit and check for two of the same of any digit
+//loop thru the array if you have three matches and two matches you win 
+// define a pair and 
+// define three of a kind 
+// if arrays has pair and three.. boom you win
+// Output: a wordCrypto that says full house or not
+//
 // a) Create a test with expect statements using the variable provided.
+//FAIL  ./code-challenges.test.js
+describe("fullHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        const hand2 = [5, 5, 3, 3, 4]
+        const hand3 = [5, 5, 5, 5, 4]
+        const hand4 = [7, 2, 7, 2, 7]
+        expect(fullHouse(hand1)).toEqual("true");
+        expect(fullHouse(hand2)).toEqual("false");
+        expect(fullHouse(hand3)).toEqual("false");
+        expect(fullHouse(hand4)).toEqual("true");
+    });
+  });
+//  b) Create the function that makes the test pass.
+ const fullHouse = (array) =>{
+  let  newVar = array.sort()                                  //sort the array and store in new variable
+  for(i = 0; i < array.length; i++)                           //for loop iterates thru sorted array
+  if(newVar[0] == newVar[1] && newVar[0] == newVar[2] && newVar[3] == newVar[4]){        //check to see if first 3 match and last two match 
+    return "true"
+  } else if (newVar[0] == newVar[1] && newVar[4] == newVar[3] && newVar[2] == newVar[3]){    //another check to see if first two match and last three match 
+    return "true"
+  } else {                                                    //if the two conditions above not met return false
+    return "false" 
+  }
+}
 
-const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
-
-// b) Create the function that makes the test pass.
--- interview-questions.md
+++ interview-questions.md
@@ -6,46 +6,64 @@ First, without external resources. Challenge yourself to answer from memory.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-1. What is a hash in Ruby?
+1. What is a hash in Ruby? 
 
-Your answer:
+Your answer: In Ruby a hash is the code between the do and end block. It can have a methods inside and is treated as an object with key value pairs.
 
-Researched answer:
+Researched answer: When working with Ruby programming language I found it very interesting the approach of OOP. I can appreciate the syntax and the hash just make sense as it represents an object. Unlike arrays hashes in Ruby do not have a zero index. Instead they make reference to the object itself. Also known as a collection or library of key value pairs. Can take in a different range of data.
 
 2. What is a gem?
+Your answer: A gem is a node module or snippets of data that allows us to run Ruby code. When running Ruby the gems are loaded into the app we create and saves us the time of having to seek these  or create them individually. 
 
-Your answer:
 
-Researched answer:
+Researched answer: They are really interesting plug ins or add ons to the Ruby library. It can allow you to add more functionality to your code. We used a ruby module called RSPEC that allowed for testing of Ruby code. Depending on what you are trying to code you can look for a gem to see if someone has built it. If not you can publish your own. There is a Ruby gems for validating things like customer location, email ect.
 
-3. What is Ruby on Rails?
+3. What is Ruby on Rails? 
 
-Your answer:
+Your answer:   Rails is a library or framework that works in conjunction with Ruby to quickly start a project using pre installed codes. Very similar to React. I have used rails to spin up an app create objects and make tables. Also was able to use SQL to run tests on top of that to make sure code is getting the return we expect.
 
-Researched answer:
+Researched answer: Rails is an MVC or Model View Controller. In my experience using this MVC it had many files but the ones we had to actually code were not that many. It was very easy to get up and running. It has a good amount of folders and files setup so that when you run it you get a web page that is running from the start. 
 
 4. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is used to convert or translate code from one language to another. SQL is a open sourced relational database. There are also operational databases or functional. I used the postrgreSQL database to create tables gather and assimilate data.
 
-Researched answer:
+Researched answer: Uses tables and rows to generate data driven analysis. Very useful in large data management, sales, marketing. Users data can be stored and accessed using primary keys. 
 
 5. What are primary keys? Why are they important?
 
-Your answer:
+Your answer:The primary keys are unique identifiers for objects in Ruby. This makes it so that each object created from a class can be unique. We created classes in ruby and used those to make objects and input or store data such as user, username and password. They are important in being able to distinguish objects.
 
-Researched answer:
+Researched answer: These are automatically done whenever a table is created. THey use an integer column. 
 
-## Looking Ahead: Terms for Next Week
-
+## Looking Ahead: Terms for Next We
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: Based on Rest architecture. Provides mapping from HTTP actions to CRUD actions
 
-2. JSON:
+2. JSON:JavaScript Object Notation
+    It is a text based way of storing and sending data. 
+    It is easy to understand in that it is self describing.
+3. ERB: Easy to operate but strong template system for RUby. You can embed Ruby code into an HTML doc. 
 
-3. ERB:
+4. Params: It is method. It returns some object like a controller and it is similar to a as. 
 
-4. Params:
+5. API:Application Programming Interface. Similar to a batch of URL's you can ask from your Rails app to retrieve data. An example of an API is an IP Geolocation service that can find out where a user is at. It gives the web page some really enhanced interface. 
 
-5. API:
+# Interview Question From Last week
+# Make a function that takes in a string and outputs the middle character of a string. If it is even output should be two characters 
+ ```Ruby
+ 
+ string= "allllloolllllla"
+ string2= "heyy"
+
+def middleLetters(str)                              #function that takes in a string 
+  middle =  str.length / 2                          #declare variable for middle by dividing string in half
+    if str %2 == 0                                    #
+        return str[middle-1] +str[middle]
+    else 
+       return str[middle.round]
+    end
+end
+
+p middleLetters(string)
